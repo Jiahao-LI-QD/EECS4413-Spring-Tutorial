22ba1a352f38167bb8b5d09b61b4896e9128f80c
jiahao <jiahaoli2017@gmail.com>
2023-11-26 17:26:51 -0500

    cleaned up for toturial

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -10,46 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/customers")
-@AllArgsConstructor
 public class CustomerController {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerController.class, args);
-	}
-
-	private final CustomerService customerService;
-
-	@GetMapping
-	public List<Customer> getCustomers(){
-		return this.customerService.findAllCustomers();
-	}
-
-	record NewCustomerRequest(String name, String email, Integer age){}
-
-	@PostMapping
-	public void addCustomer(@Valid @RequestBody NewCustomerRequest request){
-		Customer customer = new Customer();
-		customer.setName(request.name());
-		customer.setAge(request.age());
-		customer.setEmail(request.email());
-		customerService.saveCustomer(customer);
-	}
-
-	@DeleteMapping("{customerId}")
-	public void deleteCustomer(@PathVariable("customerId") Integer id){
-		customerService.deleteCustomerById(id);
-	}
-
-	@PutMapping("{customerId}")
-	public void updateCustomer(@PathVariable("customerId") Integer id, @Valid @RequestBody NewCustomerRequest request){
-		Customer customer = new Customer();
-		customer.setId(id);
-		customer.setName(request.name());
-		customer.setAge(request.age());
-		customer.setEmail(request.email());
-		customerService.saveCustomer(customer);
 	}
 
 }
