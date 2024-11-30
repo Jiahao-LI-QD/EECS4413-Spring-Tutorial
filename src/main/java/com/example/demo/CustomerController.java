ef32aa66997d8bcc4cb77936372e6f965d5a07f9
jiahao <jiahaoli2017@gmail.com>
2024-11-30 14:59:44 -0500

    clean up controller

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,13 +1,9 @@
 package com.example.demo;
 
-import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @SpringBootApplication
 public class CustomerController {
